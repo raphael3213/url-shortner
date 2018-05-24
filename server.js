7348ab6d533c715de9c1d5254c4935896e87ff4f
Glitch (absorbing-william) <none>
2018-05-24 16:21:22 +0000

    💰👚 Checkpoint ./server.js:1174538/118

--- server.js
+++ server.js
@@ -5,16 +5,18 @@
 var express = require('express');
 var app = express();
 var mongodb=require('mongodb').MongoClient
-
+var mongourl=process.env.MONGOLAB_URI;
 var arr=[];
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
-// http://expressjs.com/en/starter/basic-routing.html
+
 app.get("/:urls", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
   var url=request.params.urls;
+  mongo
+  
   if(url.length==4)
   {
     
